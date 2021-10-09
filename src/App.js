3a50e852d39f84f7b645bca17459e94db8ea83b6
alexarce <a.l.e.x_91@hotmail.com>
2021-10-09 20:24:40 -0300

    Añadiendo NadBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+import NavBar from './components/NavBar.jsx';
+
 function App() {
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    < NavBar/>
+    <h1> Plantastik </h1>
+    <br/><br/><br/>
+    <p> Tieda: especializada en venta de bulbos florales, semillas y articulos de jardin </p>
+    
+
     </div>
   );
 }
